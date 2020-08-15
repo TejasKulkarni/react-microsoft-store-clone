0b643f18b805ca8711bfea464672953a5fb3228d
Tejas Kulkarni <TejasKulkarni@users.noreply.github.com>
2020-08-15 15:17:15 +1000

    Updates

---
+++ precache-manifest.f7e767d0dfd4c7990c0e2cde13758109.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f6ed6aa30ab8ccf9e064282610b41d4",
+    "url": "/react-microsoft-store-clone/index.html"
+  },
+  {
+    "revision": "ab09e0e1488150dfd745",
+    "url": "/react-microsoft-store-clone/static/css/main.001b0961.chunk.css"
+  },
+  {
+    "revision": "1b98af754058b8e36853",
+    "url": "/react-microsoft-store-clone/static/js/2.8ba64989.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-microsoft-store-clone/static/js/2.8ba64989.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab09e0e1488150dfd745",
+    "url": "/react-microsoft-store-clone/static/js/main.52a93bb0.chunk.js"
+  },
+  {
+    "revision": "df2caff67a6cafc11ebc",
+    "url": "/react-microsoft-store-clone/static/js/runtime-main.a8e40325.js"
+  }
+]);
