5ad5d192e0f58b576271a0c95c2829587e1a32a1
Tejas Kulkarni <TejasKulkarni@users.noreply.github.com>
2020-08-15 18:46:50 +1000

    Updates

---
+++ precache-manifest.0a914c96eb0e96cb52493334b1ccdd59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1574eb2cd6691ef1bc21a0c508bcd18",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ab09e0e1488150dfd745",
+    "url": "./static/css/main.001b0961.chunk.css"
+  },
+  {
+    "revision": "1b98af754058b8e36853",
+    "url": "./static/js/2.8ba64989.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.8ba64989.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab09e0e1488150dfd745",
+    "url": "./static/js/main.52a93bb0.chunk.js"
+  },
+  {
+    "revision": "5be62e8a5268b5996200",
+    "url": "./static/js/runtime-main.011b21b7.js"
+  }
+]);
